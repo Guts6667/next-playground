c075ed3fd79979b75f10574c585eb1ee6c2992e0
Guts 6667 <chambet.dev@gmail.com>
2023-03-10 12:05:42 +0100

    V.0.3
    SSR vs CSR

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,29 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 // import Link from "next/link";
-import { getFeaturedEvents } from "../../dummy-data";
-import EventList from "@/components/events/event-list";
-
+import { GetStaticProps } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  /** We use Link from next the same we use <a/>
-   * Link has a special attribute: when the user is hovering a link, all the content of a page is preloaded
-   * Link also has a few specif attributes such as: "replace" which allows to replace the current page
-   * by the targeted one so it becomes impossible to come back to the previous one.
-   */
+const HomePage = (props) => {
+const { products } = props;
 
-  const featuredEvents = getFeaturedEvents();
   return (
     <div className={styles.main}>
       <h1>HOME PAGE: NEXT PLAYGROUND</h1>
-      <EventList datas={featuredEvents} />
+      <ul>
+        {products.map((product) =>{
+          return(
+            <li key={product.id}></li>
+          )
+        } )}
+      </ul>
     </div>
   );
 }
+
+export async function  GetStaticProps(){
+  return { props: {
+    products: [{id: "p1", title: "Product1"}],
+  } }
+}
+export default HomePage;
